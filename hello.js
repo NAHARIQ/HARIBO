300eb2dd1226201b7978b3d4dc6b1964226865ef
NAHARIQ <nahari.will@gmail.com>
2022-10-04 02:18:16 +0900

    Let, For, Function 등등

---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log("hello");
+let hello = "안녕하세요";
+console.log(hello);
+for (let i=9; i>-5; i--){
+    console.log(i)
+}
+
+let ttt = (n,m)=>{
+    console.log(n,m)
+}
+
+ttt(1,2);
+function ttt2(n,m){
+    console.log(n,m)
+}
+
+ttt2(1,2);
+ttt2(3,4);
